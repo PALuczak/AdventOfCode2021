5c4fb2db22cf9fd9e61327a47477fad19bc122e8
Piotr Łuczak <pluczak@iis.p.lodz.pl>
2021-12-13 18:11:44 +0100

    Day 1 B

--- day1.py
+++ day1.py
@@ -8,3 +8,15 @@ with open("day1.txt", "r") as infile:
         prev = val
 
 print("PART A", increases)
+
+prev = -1
+increases = 0
+with open("day1.txt", "r") as infile:
+    vals = [int(val) for val in infile]
+
+    for x in zip(vals, vals[1:], vals[2:]):
+        val = sum(x)
+        if prev != -1 and val > prev:
+            increases += 1
+        prev = val
+print("PART B", increases)
